890f04ce9a2200c574601890b8e795c797c47686
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-23 22:31:22 -0600

    Loading data

--- main.py
+++ main.py
@@ -13,3 +13,49 @@ import matplotlib.pyplot as plt
 from tensorflow import image as tf_image
 from tensorflow import data as tf_data
 from tensorflow import io as tf_io
+
+# Creating dataset
+IMAGE_SIZE = 512
+BATCH_SIZE = 4
+NUM_CLASSES = 20
+DATA_DIR = os.path.join(os.path.expanduser('~'), 'data', 'instance-level_human_parsing', 'instance-level_human_parsing', 'Training')
+NUM_TRAIN_IMAGES = 1000
+NUM_VAL_IMAGES = 50
+
+train_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[:NUM_TRAIN_IMAGES]
+train_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[:NUM_TRAIN_IMAGES]
+val_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[
+    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
+]
+val_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[
+    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
+]
+
+def read_image(image_path, mask=False):
+    image = tf_io.read_file(image_path)
+    if mask:
+        image = tf_image.decode_png(image, channels=1)
+        image.set_shape([None, None, 1])
+        image = tf_image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
+    else:
+        image = tf_image.decode_png(image, channels=3)
+        image.set_shape([None, None, 3])
+        image = tf_image.resize(images=image, size=[IMAGE_SIZE, IMAGE_SIZE])
+    return image
+
+def load_data(image_list, mask_list):
+    image = read_image(image_list)
+    mask = read_image(mask_list, mask=True)
+    return image, mask
+
+def data_generator(image_list, mask_list):
+    dataset = tf_data.Dataset.from_tensor_slices((image_list, mask_list))
+    dataset = dataset.map(load_data, num_parallel_calls=tf_data.AUTOTUNE)
+    dataset = dataset.batch(BATCH_SIZE, drop_remainder=True)
+    return dataset
+
+train_dataset = data_generator(train_images, train_masks)
+val_dataset = data_generator(val_images, val_masks)
+
+print("Train Dataset:", train_dataset)
+print("Val Dataset:", val_dataset)
