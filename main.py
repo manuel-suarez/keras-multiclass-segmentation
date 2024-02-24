75f1db88ffb1f15943cbdc960f99e60d87913e35
Arturo Suárez <manuel.suarez@cimat.mx>
2024-02-24 00:51:19 -0600

    Load complete dataset

--- main.py
+++ main.py
@@ -20,18 +20,16 @@ from tensorflow import io as tf_io
 IMAGE_SIZE = 512
 BATCH_SIZE = 4
 NUM_CLASSES = 20
-DATA_DIR = os.path.join(os.path.expanduser('~'), 'data', 'instance-level_human_parsing', 'instance-level_human_parsing', 'Training')
+DATA_DIR = os.path.join(os.path.expanduser('~'), 'data', 'instance-level_human_parsing', 'instance-level_human_parsing')
+TRAIN_DIR = os.path.join(DATA_DIR, 'Training')
+VAL_DIR = os.path.join(DATA_DIR, 'Validation')
 NUM_TRAIN_IMAGES = 1000
 NUM_VAL_IMAGES = 50
 
-train_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[:NUM_TRAIN_IMAGES]
-train_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[:NUM_TRAIN_IMAGES]
-val_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[
-    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
-]
-val_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[
-    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
-]
+train_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))
+train_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))
+val_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))
+val_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))
 
 def read_image(image_path, mask=False):
     image = tf_io.read_file(image_path)
